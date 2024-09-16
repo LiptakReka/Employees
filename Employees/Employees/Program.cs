0bded2f0c092498e253e25923ceb9c44f7ae66ec
LiptakReka <liptakr@kkszki.hu>
2024-09-16 12:09:37 +0200

    System.Io added

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Employees
 {
@@ -10,6 +11,7 @@ namespace Employees
     {
         static void Main(string[] args)
         {
+           
         }
     }
 }
