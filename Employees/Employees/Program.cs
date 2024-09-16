1feda984949989695a6392609703b3528e33d252
LiptakReka <liptakr@kkszki.hu>
2024-09-16 12:34:58 +0200

    File readed and imported the text file

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -4,17 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel;
 
 namespace Employees
 {
     internal class Program
     {
-        List<string> list = new List<string>();
+         static List<string> listaadat = new List<string>();
         static void Main(string[] args)
         {
+           string[] adatok= File.ReadAllLines("tulajdonsagok_100sor.txt");
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string id = adatok[i].Split(';')[0];
+                string nev = adatok[i].Split(';')[1];
+                string kor = adatok[i].Split(';')[2];
+                string fzetes = adatok[i].Split(';')[3];
 
-          
-          
+                listaadat.Add(id);
+                listaadat.Add(nev);
+                listaadat.Add(kor);
+                listaadat.Add(fzetes);
+            }
+            
+
         }
     }
 }
